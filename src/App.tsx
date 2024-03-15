c70494429b57726df3e33fbbd8aff7ce0405d976
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-03-15 16:46:32 +0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>Privacy Policy Healty app</h1>
+			<div className="card">
+				<h2> Introduction</h2>
+
+				<p>
+					Thank you for choosing our App. We are committed to the security and
+					privacy of your personal information. In this privacy policy
+					(hereinafter referred to as the “Policy”), we describe how we collect,
+					use, disclose, and protect your personal information.
+				</p>
+
+				<h2>Data Collection and Use</h2>
+				<p>
+					We do not collect or use any personal data or information about you.
+					This includes your personal identifiers such as name, address, phone
+					number, email address, or location.
+				</p>
+
+				<h2>Data Disclosure</h2>
+
+				<p>
+					Your consent is a prerequisite for the transfer, sale, exchange, or
+					other disclosure of your personal data. We will not disclose your
+					personal data without your consent.
+				</p>
+
+				<h2>Data Security and Protection</h2>
+				<p>
+					We take all necessary technical and organizational measures to protect
+					your personal data from unauthorized access, use, or modification.
+				</p>
+
+				<h2>Changes to the Policy</h2>
+
+				<p>
+					We reserve the right to make changes to this Policy. If we make
+					significant changes, we will publish a new version of the Policy on
+					the App’s Google Play Store page.
+				</p>
+
+				<h2>Conclusion</h2>
+
+				<p>
+					Please read our Privacy Policy carefully and ask questions if
+					something is not clear.
+				</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
